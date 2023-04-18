12aa83035b8ab5afaadf55861f1e10720ba01998
tommyg141 <tommymordhi@gmail.com>
2023-04-18 22:30:10 +0300

    fix3

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,31 +9,19 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pos_up_down = new Vector3(0,0.09f,0);
-        pos_right_left = new Vector3(0.09f,0,0);
+    pos_up_down = new Vector3(0,0.09f,0);
+    pos_right_left = new Vector3(0.09f,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            GetComponent<Transform>().position += pos_up_down;
-        }
-
+        if (Input.GetKey(KeyCode.UpArrow)){GetComponent<Transform>().position += pos_up_down;}
         if (Input.GetKey(KeyCode.DownArrow))
-        {
-            GetComponent<Transform>().position -= pos_up_down;
-
-        }
+        {GetComponent<Transform>().position -= pos_up_down;}
         if (Input.GetKey(KeyCode.RightArrow))
-        {
-            GetComponent<Transform>().position += pos_right_left;
-        }
-
+        {GetComponent<Transform>().position += pos_right_left;}
         if (Input.GetKey(KeyCode.LeftArrow))
-        {
-           GetComponent<Transform>().position -= pos_right_left;
-        }
+        {GetComponent<Transform>().position -= pos_right_left;}
     }
 }
--- Assets/Scripts/MoverLeft.cs
+++ Assets/Scripts/MoverLeft.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class MoverLeft : MonoBehaviour
 {
-[Tooltip("Movement vector in meters per second")]
-[SerializeField] Vector3 velocity;
+ [Tooltip("Movement vector in meters per second")]
+ [SerializeField] Vector3 velocity;
     //i want to rand speed to my velocity
     // Start is called before the first frame update
-    void Start()
-    {
-    velocity = velocity * Random.Range(1,5);
-    }
-void Update() {
-        transform.position += velocity * Time.deltaTime;
-    }
-public void SetVelocity(Vector3 newVelocity) {
-        this.velocity = newVelocity;
-    }
+ void Start(){velocity = velocity * Random.Range(1,5);}
+ void Update() {transform.position += velocity * Time.deltaTime;}
+ public void SetVelocity(Vector3 newVelocity) {this.velocity = newVelocity;}
 }
